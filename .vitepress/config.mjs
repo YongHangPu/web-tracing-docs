4003e6496cfb3a57ae8494ea4c0272b26358cfe7
yonghang <1251679661@qq.com>
2025-02-19 15:38:54 +0800

    docs: 初始化 Vitepress 文档项目

---
+++ .vitepress/config.mjs
@@ -0,0 +1,67 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  lang: 'zh-CN',
+  title: 'web-tracing',
+  description: '行为埋点 & 性能采集 & 异常采集 & 请求采集 & 路由采集',
+  base: '/web-tracing',
+  cleanUrls: true,
+  appearance: 'dark',
+  themeConfig: {
+    logo: '/logo.svg',
+    // 开启本地搜索
+    search: {
+      provider: 'local'
+    },
+    // 修改侧边栏导航的标题
+    outline: {
+      label: '页面导航',
+      level: [1, 4]
+    },
+    // 修改文档页脚的文字
+    docFooter: {
+      prev: '上一页',
+      next: '下一页'
+    },
+    lightModeSwitchTitle: '切换到浅色主题',
+    darkModeSwitchTitle: '切换到深色主题',
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '指南', link: '/guide/starting' },
+      { text: '关于项目', link: '/guide/spotlight' },
+      { text: '配置项', link: '/guide/use/options' }
+    ],
+    sidebar: [
+      {
+        text: '指南',
+        items: [
+          { text: '起步', link: '/guide/starting' },
+          { text: '最佳实践', link: '/guide/practice' },
+          { text: '关于项目', link: '/guide/spotlight' }
+        ]
+      },
+      {
+        text: '使用',
+        items: [
+          { text: '基础说明', link: '/guide/use/declare' },
+          { text: '配置项', link: '/guide/use/options' },
+          { text: '数据结构', link: '/guide/use/structure' }
+        ]
+      },
+      {
+        text: '功能',
+        items: [
+          { text: '事件采集', link: '/guide/functions/event' },
+          { text: '错误采集', link: '/guide/functions/error' },
+          { text: '路由采集', link: '/guide/functions/pv' },
+          { text: '请求采集', link: '/guide/functions/http' },
+          { text: '资源采集', link: '/guide/functions/performance' },
+          { text: '曝光采集', link: '/guide/functions/intersection' },
+          { text: '导出项', link: '/guide/functions/exports' }
+          // { text: '其他', link: '/guide/functions/other' },
+        ]
+      }
+    ]
+  }
+})
---
+++ index.md
@@ -0,0 +1,34 @@
+---
+layout: home
+
+title: WebTracing
+titleTemplate: 埋点
+
+hero:
+  name: WebTracing
+  text: 为前端项目提供完善的监控手段
+  actions:
+    - theme: brand
+      text: 起步
+      link: /guide/starting
+    - theme: alt
+      text: 关于项目
+      link: /guide/spotlight
+    - theme: alt
+      text: 使用
+      link: /guide/use/declare
+
+features:
+  - title: 功能丰富
+    details: 足以应对大部分前端项目的监控需求
+    icon: 🚀
+  - title: 面面俱到
+    details: 目前已适配 [ js、vue2、vue3 ]
+    icon: ⚡
+  - title: 随机应变
+    details: 提供多种拦截方法、配置项动态更改
+    icon: 🛠
+  - title: 珠联璧合
+    details: demo、文档、sdk核心功能 于一体
+    icon: 🎪
+---
